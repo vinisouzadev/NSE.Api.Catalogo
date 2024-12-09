22d784ef7ab32aaa605d25de57aa6172524c95b9
Vinícius Souza <viniciusouza.dev@gmail.com>
2024-12-09 16:49:30 -0300

    Configuração inicial de CatalogoContext

---
+++ Data/CatalogoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.API.Catalogo.Models;
+
+namespace NSE.API.Catalogo.Data
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
