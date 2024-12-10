990698bbaf7811f8d738ca5e4c7b8c268cc5b3b5
Vinícius Souza <viniciusouza.dev@gmail.com>
2024-12-10 09:18:37 -0300

    Cria a controller 'CatalogoController' e implementa seus endpoints

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.API.Catalogo.Data.Repository;
+using NSE.API.Catalogo.Models;
+
+namespace NSE.API.Catalogo.Controllers
+{
+    [ApiController]
+    [Route("catalogo/produtos/")]
+    public class CatalogoController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CatalogoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Produto>> Index()
+        {
+            return await _produtoRepository.ObterTodos();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Produto> ProdutoDetalhe(Guid id)
+        {
+            return await _produtoRepository.ObterPorId(id);
+        }
+    }
+}
