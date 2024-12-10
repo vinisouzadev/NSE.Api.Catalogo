5e6820c800f351723a0d60159a8e31f9f7e6230a
Vinícius Souza <viniciusouza.dev@gmail.com>
2024-12-10 09:56:43 -0300

    Adiciona métodos de extensão no Program

--- Program.cs
+++ Program.cs
@@ -1,24 +1,15 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using NSE.API.Catalogo.Configuration;
-using NSE.API.Catalogo.Data;
-using NSE.API.Catalogo.Data.Repository;
-using NSE.API.Catalogo.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder
     .AddApiConfiguration()
     .AddDependencyInjection();
-
-
 builder.Services.AddEndpointsApiExplorer();
+builder.AddSwagger();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
